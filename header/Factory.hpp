f51342a3956730702ac855608b8a06ab9aea84de
nkadekar <nkadekar@github.com>
2020-11-19 15:15:05 -0800

    Initial Code for Factory.hpp

---
+++ header/Factory.hpp
@@ -0,0 +1,16 @@
+#ifndef __FACTORY_HPP__
+#define __FACTORY_HPP__
+
+#include "base.hpp"
+#include <string>
+
+using namespace std;
+
+class Factory {
+    public:
+        Base* parse(){
+
+        }
+};
+
+#endif
